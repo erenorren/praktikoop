ab0c798b5c0ed0fee72cb903cff737dbac295371
erenorren <regishashrn07@gmail.com>
2025-08-29 11:25:13 +0700

    Create home.php

---
+++ home.php
@@ -0,0 +1,10 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Halaman Beranda</title>
+</head>
+<body>
+    <h1>Selamat Datang di Halaman Beranda</h1>
+    <p>Klik <a href="index.php">di sini</a> untuk menuju ke halaman utama.</p>
+</body>
+</html>
