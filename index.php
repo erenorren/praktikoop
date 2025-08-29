1572215d38abefdf358406421fbc3b3574cde730
erenorren <regishashrn07@gmail.com>
2025-08-29 11:24:53 +0700

    Create index.php

---
+++ index.php
@@ -0,0 +1,9 @@
+<html>
+  <head>
+    <title>PHP Test</title>
+  </head>
+  <body>
+    <?php echo '<p>Selamat datang dipraktikum PBO semester 3: Sheren</p>'; ?> 
+    <a href="home.php">Go To Home</a>
+
+</html>
